4f2df3526007470ca52b7369824217ec0a5fc68f
CarterWilde <carterjwilde@gmail.com>
2021-09-14 14:38:49 -0600

    Added Card Style

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,15 +1,16 @@
-import { HTMLAttributes} from "react";
+import { CSSProperties, HTMLAttributes} from "react";
 import "./Card.css";
 
 type CardProps = {
     className?: HTMLAttributes<HTMLDivElement>['className'],
     children?: React.ReactNode,
-    onClick?: HTMLAttributes<HTMLDivElement>['onClick']
+    onClick?: HTMLAttributes<HTMLDivElement>['onClick'],
+    style?: CSSProperties
 };
 
 const Card = (props : CardProps) => {
     return(
-        <div className={`card` + (props.className ? ' ' + props.className : '' )} onClick={props.onClick} >
+        <div className={`card` + (props.className ? ' ' + props.className : '' )} onClick={props.onClick} style={props.style}>
             {props.children}
         </div>
     );
