7987cefe7454ea8d1de2ea1c8703c313d00b1b18
CarterWilde <carterjwilde@gmail.com>
2021-09-15 14:03:15 -0600

    Better Button Styles

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,27 @@
+import React, { CSSProperties, ReactNode } from "react";
+
+import "./Button.css";
+
+type ButtonProps  = {
+    children?: ReactNode,
+    variant?: "text" | "outline" | "primary" | "contained" | "filled",
+    onClick?: React.MouseEventHandler<HTMLDivElement>,
+    color?: string
+};
+
+export class InvalidVariantError extends Error {
+    constructor(msg: string) {
+        super(msg);
+        this.name = "InvalidVariantError";
+    }
+}
+
+const Button = (props: ButtonProps) => {
+    return(
+        <div className={"btn" + (props.variant ? ` ${props.variant}` : ' primary')} onClick={props.onClick} style={{cursor: "pointer", width: "fit-content", color: props.color}}>
+            {props.children}
+        </div>
+    );
+}
+
+export default Button;
