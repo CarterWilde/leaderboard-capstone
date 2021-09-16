35776cc01016db444b550e25eaaa1e7ab343406a
CarterWilde <carterjwilde@gmail.com>
2021-09-16 14:56:42 -0600

    TextedIcons Added

---
+++ src/Components/TextedIcon/TextedIcon.tsx
@@ -0,0 +1,24 @@
+import { SvgIconProps } from "@material-ui/core";
+import { SvgIconComponent } from "@material-ui/icons";
+import { CSSProperties, ReactElement } from "react";
+
+import "./TextedIcon.css";
+
+export type TextedIconProps = {
+    className?: string;
+    icon: ReactElement<SvgIconProps, SvgIconComponent>;
+    children: string;
+    style?: CSSProperties;
+    size?: string;
+}
+
+const TextedIcon = (props: TextedIconProps) => {
+    return(
+        <div className={"texted-icon" + (props.className ? ` ${props.className}` : '')} style={{height: `calc(${props.size} + .5em)`, fontSize: props.size, ...props.style}}>
+            {props.icon}
+            <p>{props.children}</p>
+        </div>
+    );
+}
+
+export default TextedIcon
