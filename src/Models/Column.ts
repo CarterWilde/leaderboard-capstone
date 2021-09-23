60cf37a12d576bbd60360a9315fea2d00228b5fe
CarterWilde <carterjwilde@gmail.com>
2021-09-22 18:12:45 -0600

    Better Column Type

--- src/Models/Column.ts
+++ src/Models/Column.ts
@@ -3,6 +3,6 @@ import Category from "./Category";
 export default interface Column {
     id: string;
     name: string;
-    type: string;
+    type: "string" | "int" | "double" | "float" | "decimal" | "uint" | "long" | "ulong" | "short" | "ushort" | "byte" | "sbyte" | "boolean" | "char"  | "duration" | "date" | "vod";
     category?: Category['id']
 }
