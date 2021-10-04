fbdce9599ac2f64e42725f97cb1a312a0ea984ae
CarterWilde <carterjwilde@gmail.com>
2021-10-04 13:55:31 -0600

    Started Web Api

---
+++ SpeedRunningLeaderboardsWebApi/Controllers/GamesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SpeedRunningLeaderboards.Repositories;
+
+namespace SpeedRunningLeaderboardsWebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class GamesController : ControllerBase
+	{
+		private readonly GameRepository _repo;
+		public GamesController(GameRepository repo)
+		{
+			_repo = repo;
+		}
+		[HttpGet]
+		public IActionResult GetGames()
+		{
+			return Ok(_repo.Get());
+		}
+		[HttpGet]
+		public IActionResult GetGame(Guid gameId)
+		{
+			return Ok(_repo.Get(gameId));
+		}
+	}
+}
---
+++ SpeedRunningLeaderboardsWebApi/Controllers/ServersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using SpeedRunningLeaderboards.Repositories;
+
+namespace SpeedRunningLeaderboardsWebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ServersController : ControllerBase
+	{
+		private readonly ServerRepository _repo;
+		public ServersController(ServerRepository repo)
+		{
+			_repo = repo;
+		}
+
+		[HttpGet]
+		public IActionResult GetServers()
+		{
+			return Ok(_repo.Get());
+		}
+		[HttpGet("@me")]
+		public IActionResult GetMyServers()
+		{
+			return Ok(_repo.Get());
+		}
+	}
+}
