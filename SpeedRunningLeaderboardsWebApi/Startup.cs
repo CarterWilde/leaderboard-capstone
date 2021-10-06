33125bfaf194d37ccdc81e8547ac10ec2dfcd240
CarterWilde <carterjwilde@gmail.com>
2021-10-05 22:06:33 -0600

    Added Logging and HTTPClient

--- SpeedRunningLeaderboardsWebApi/Startup.cs
+++ SpeedRunningLeaderboardsWebApi/Startup.cs
@@ -30,6 +30,13 @@ namespace SpeedRunningLeaderboardsWebApi
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddLogging(logging => {
+				logging.ClearProviders();
+				logging.AddConsole();
+			});
+
+			services.AddHttpClient();
+
 			services.AddSingleton<DapperContext>();
 			services.AddScoped<ServerRepository>();
 			services.AddScoped<GameRepository>();
