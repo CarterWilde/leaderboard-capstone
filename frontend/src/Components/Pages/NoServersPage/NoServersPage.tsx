365815b350532f8058842e39324bbadac28b1ea6
CarterWilde <carterjwilde@gmail.com>
2021-10-11 09:37:29 -0600

    Added NoServersPage

--- frontend/src/Components/Pages/NoServersPage/NoServersPage.tsx
+++ frontend/src/Components/Pages/NoServersPage/NoServersPage.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import BackgroundEffect from "../../BackgroundEffect";
+import { CreateServer, JoinServer } from "../../PopUps";
 import { Button, PopUp } from "../../UI";
 import "./NoServersPage.css";
 
 const NoServersPage = () => {
+	const [openJoinServer, setOpenJoinServer] = useState(false);
+	const [openCreateServer, setOpenCreateServer] = useState(false)
 	return(
 		<section className="no-servers page" style={{height: "100%"}}>
-			<PopUp open isBlank>
+			<JoinServer open={openJoinServer} onClosed={() => {
+				setOpenJoinServer(false);
+			}}/>
+			<CreateServer open={openCreateServer} onClosed={() => {
+				setOpenCreateServer(false);
+			}}/>
+			<PopUp open={!openJoinServer && !openCreateServer} isBlank className="no-servers">
 				<h3>You're not part of any servers</h3>
 				<hr/>
 				<section>
 					<div>
-						<Button variant="inline">Join a server</Button>
+						<Button variant="inline" onClick={() => {setOpenJoinServer(true)}}>Join a server</Button>
 						&nbsp;or&nbsp;
-						<Button variant="inline">create a server!</Button>
+						<Button variant="inline" onClick={() => {setOpenCreateServer(true)}}>create a server!</Button>
 					</div>
 				</section>
 			</PopUp>
