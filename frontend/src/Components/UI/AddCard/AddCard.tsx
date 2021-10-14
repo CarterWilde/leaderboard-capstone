df2f04dbd23caa0c84394ef847ffc851a04e9315
CarterWilde <carterjwilde@gmail.com>
2021-10-14 16:52:48 -0600

    Added AddCard OnClick

--- frontend/src/Components/UI/AddCard/AddCard.tsx
+++ frontend/src/Components/UI/AddCard/AddCard.tsx
@@ -1,9 +1,10 @@
 import { Add } from "@material-ui/icons";
+import { HTMLAttributes } from "react";
 import { Card } from "..";
 import "./AddCard.css";
 
-export const AddCard = (props: {className?: string}) => (
-	<Card className={"add flair-hover " +  (props.className ? props.className : "")}>
+export const AddCard = (props: {className?: string, onClick?: HTMLAttributes<HTMLDivElement>['onClick']}) => (
+	<Card className={"add flair-hover " +  (props.className ? props.className : "")} onClick={props.onClick}>
 		<Add />
 	</Card>
 );
